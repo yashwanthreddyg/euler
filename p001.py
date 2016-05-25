70a32e075546a1599616928edf31d7aa9fdb2961
Yashwanth Reddy Gondi <yashreddy.g@gmail.com>
2016-05-25 17:24:49 +0530

    added second prob

--- p001.py
+++ p001.py
@@ -1,4 +1,6 @@
-set3 = {x for x in range(1,1000) if x%3 == 0}
-set5 = {x for x in range(1,1000) if x%5==0}
-finalset = ((set3-set5)|(set5 - set3)) | (set3 & set5)
-print sum(finalset)
+# set3 = {x for x in range(1,1000) if x%3 == 0}
+# set5 = {x for x in range(1,1000) if x%5==0}
+# finalset = ((set3-set5)|(set5 - set3)) | (set3 & set5)
+# print sum(finalset)
+arr = [x for x in range(1,1000) if x % 3 == 0 or x%5 == 0]
+print sum(arr)
